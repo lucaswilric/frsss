270d86478b4b6b2d33c3d8f92a9a9d25989f803d
Lucas Wilson-Richter <da.maestro@gmail.com>
2012-08-13 22:48:25 +1000

    Make it do something useful

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'http://rubygems.org'
 
+gem 'bundler'
 gem 'sinatra'
+gem 'heroku'
 gem 'haml'
+gem "faraday", "~> 0.8.2"
+gem 'ruby-xslt'
--- app.rb
+++ app.rb
@@ -1,5 +1,18 @@
+require 'rexml/document'
+require 'xml/xslt'
 
 get '/' do
-  "Hi!"
+  response = Faraday.get 'http://grabbit.lucasrichter.id.au/download_jobs/tagged/{TAG}/feed.rss'.gsub('{TAG}', request.host.split(/[.:]/)[0])
+  
+  rss = response.body
+  
+  response = Faraday.get 'http://assets.lucasrichter.id.au/xsl/rss.xsl'
+  xsl = response.body
+  
+  xslt = XML::XSLT.new()
+  xslt.xml = REXML::Document.new rss
+  xslt.xsl = REXML::Document.new xsl
+  
+  xslt.serve()
 end
 
